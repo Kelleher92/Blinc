8d1733ea73edc93588c5bf55e1b10967128902c6
Kelleher92 <iankelleher92@gmail.com>
2016-02-29 11:31:36 +0000

    display score

--- app/src/main/java/com/eyes/blinc/MainActivity.java
+++ app/src/main/java/com/eyes/blinc/MainActivity.java
@@ -1,7 +1,29 @@
 package com.eyes.blinc;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
 /**
  * Created by Ian on 29-Feb-16.
  */
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        EditText score = (EditText) this.findViewById(R.id.editText);
+        score.setText("87");
+    }
+
+    public void Back(View view) {
+        Intent i = new Intent(this, SplashActivity.class);
+        startActivity(i);
+        finish();
+    }
 }
+
+
